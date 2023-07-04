65695ae7ad0ead2d4cefff411f38febfb044f77f
danilalabydindfsdf <labydin2605@mail.ru>
2023-07-04 22:24:28 +0300

    add black to poetry, run black

--- src/crud.py
+++ src/crud.py
@@ -1,14 +1,14 @@
 from sqlalchemy.orm import Session
 
-import models, schemas
+from .models import User
 
 
 def get_user_by_username(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username).first()
+    return db.query(User).filter(User.username == username).first()
 
 
 def create_user(db, user):
-    db_user = models.User(username=user.username, hashed_password=user.password, promotion_date=user.promotion_date, salary=user.salary, is_active=True)
+    db_user = User(username=user.username, hashed_password=user.password, promotion_date=user.promotion_date, salary=user.salary, is_active=True)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
